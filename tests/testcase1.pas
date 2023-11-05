09c344f298e210b3472829fff03012967633944e
Matteo Salvi <salvadorbs@gmail.com>
2023-11-05 01:04:48 +0100

    test: added two methods to check jsonObject and ItemsLine

--- tests/testcase1.pas
+++ tests/testcase1.pas
@@ -1,42 +1,73 @@
 unit TestCase1;
 
-{$mode objfpc}{$H+}
+{$mode DelphiUnicode}
 
 interface
 
 uses
   Classes, SysUtils, TestFramework, mormot.core.json, mormot.core.os, mormot.core.base,
-  json.root, json.currency, json.store, json.totaltaxes, json.itemsline;
+  Json.Expense;
 
 type
-  TLidlToGrocyTestCase= class(TTestCase)
+
+  { TLidlToGrocyTestCase }
+
+  TLidlToGrocyTestCase = class(TTestCase)
   private
-    FRoot: TRootArray;
+    FExpense: TExpenseArray;
   protected
     procedure SetUp; override;
     procedure TearDown; override;
   published
-    procedure TestHookUp;
+    procedure ValidJsonObject;
+    procedure CheckItemsLine;
   end;
 
 implementation
-
-procedure TLidlToGrocyTestCase.TestHookUp;
-begin
-  Fail('Write your own test');
-end;
 
 procedure TLidlToGrocyTestCase.SetUp;
 var
   json: RawUtf8;
 begin
   json := StringFromFile('test.json');
-  DynArrayLoadJson(FRoot, json, TypeInfo(TRootArray));
+  DynArrayLoadJson(FExpense, json, TypeInfo(TExpenseArray));
 end;
 
 procedure TLidlToGrocyTestCase.TearDown;
+var
+  I: Integer;
 begin
-  //FRoot.Free;
+  for I := 0 to length(FExpense) - 1 do
+  begin
+    FExpense[I].Free;
+  end;
+end;
+
+procedure TLidlToGrocyTestCase.ValidJsonObject;
+begin
+  CheckEquals(Length(FExpense), 2);
+
+  CheckEqualsUnicodeString(FExpense[0].id, '1');
+  CheckEqualsUnicodeString(FExpense[1].id, '2');
+end;
+
+procedure TLidlToGrocyTestCase.CheckItemsLine;
+begin
+  CheckEquals(Length(FExpense), 2);
+
+  CheckEquals(Length(FExpense[0].ItemsLine), 3);
+  CheckEqualsUnicodeString(FExpense[0].ItemsLine[0].Name, 'PRODUCT1');
+  CheckEqualsUnicodeString(FExpense[0].ItemsLine[0].TaxGroupName, 'A');
+  CheckEqualsUnicodeString(FExpense[0].ItemsLine[1].Name, 'PRODUCT2');
+  CheckEqualsUnicodeString(FExpense[0].ItemsLine[1].TaxGroupName, 'B');
+  CheckEqualsUnicodeString(FExpense[0].ItemsLine[2].Name, 'PRODUCT3');
+  CheckEqualsUnicodeString(FExpense[0].ItemsLine[2].TaxGroupName, 'C');
+
+  CheckEquals(Length(FExpense[1].ItemsLine), 2);
+  CheckEqualsUnicodeString(FExpense[1].ItemsLine[0].Name, 'PRODUCT1');
+  CheckEqualsUnicodeString(FExpense[1].ItemsLine[0].TaxGroupName, 'A');
+  CheckEqualsUnicodeString(FExpense[1].ItemsLine[1].Name, 'PRODUCT2');
+  CheckEqualsUnicodeString(FExpense[1].ItemsLine[1].TaxGroupName, 'B');
 end;
 
 initialization
